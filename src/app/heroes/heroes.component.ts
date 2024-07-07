c229aeef45dc2d8baacfbb739d9cafe9e0d089ca
demetrio <dem.iaria@gmail.com>
2024-07-07 13:57:41 +0200

    generated a hero-component and a hero interface to carry a new hero type

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Supaman'
+  };
+}
