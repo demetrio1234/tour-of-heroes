326dcbb7ba35fa6710848680b6c2cd7a2720fb08
demetrio <dem.iaria@gmail.com>
2024-07-07 18:56:59 +0200

    Added a new hide property to the hero interface

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice', hide: false },
+    { id: 13, name: 'Bombasto', hide: false },
+    { id: 14, name: 'Celeritas', hide: false },
+    { id: 15, name: 'Magneta', hide: false },
+    { id: 16, name: 'RubberMan', hide: false },
+    { id: 17, name: 'Dynama', hide: false },
+    { id: 18, name: 'Dr. IQ', hide: false },
+    { id: 19, name: 'Magma', hide: false },
+    { id: 20, name: 'Tornado', hide: false },
+];
