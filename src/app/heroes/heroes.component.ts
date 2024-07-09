e11fbc72838f6d1c8340de8eda6c3d05596c5858
demetrio <dem.iaria@gmail.com>
2024-07-09 18:51:59 +0200

    Cleaned up the heroes.component.ts from unnecessary properties and methods

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -10,14 +11,13 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeroDetailComponent],
+  imports: [CommonModule, FormsModule, HeroDetailComponent, RouterModule],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
@@ -30,10 +30,5 @@ export class HeroesComponent implements OnInit {
       .subscribe(
         heroes =>
           this.heroes = heroes);
-  }
-
-  onSelect(selectedHero: Hero): void {
-    this.selectedHero = selectedHero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${selectedHero.id}`);
   }
 }
